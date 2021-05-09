ce887158b4ef240689b89a35b013cfe5ba0e2037
CHG <chiragmittal97@yahoo.co.in>
2021-05-08 23:47:13 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import CalculatorComponent from './components/CalculatorComponent';
 import LogComponent from './components/LogComponent';
